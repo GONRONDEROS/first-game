570f20265057a2c59824c2e6de2e36dc0f04f3d2
gonzalo <gonzalorond@gmail.com>
2022-09-07 15:29:52 -0300

    Establecida UI

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/////Choices/////
-let choices = ["piedra", "papel", "tijera"];
 
 ///////Total Score/////////
 let humanScore=0;
@@ -11,62 +9,107 @@ const ganar="***HAS GANADO ESTA BATALLA! AUN FALTA PARA GANAR LA GUERRA, PERO SI
 const perder="***HAS PERDIDO ESTA BATALLA. LA HUMANIDAD MORIRAAAAAAAA!!!!***";
 const empatar= "***ES UN EMPATE. INTENTA DE NUEVO SI TE ANIMAS...***";
 
+/////Choices/////
+let choices = ["piedra", "papel", "tijera"];
+
+////Computer Play////
+function computerPlay(){
+    let choices = ["piedra", "papel", "tijera"];
+    computerChoice = Math.floor(Math.random() * choices.length);
+    return choices[computerChoice];
+};
+
+/////buttons/////
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissor');
+const outcomeDiv = document.querySelector('.outcome');
+const resultDiv = document.querySelector('.results');
+const playerScoreSpan = document.querySelector('.player-score');
+const computerScoreSpan = document.querySelector('.computer-score');
+
 //// Round ////
 
 function playRound(playerSelection,  computerDecision) {
 
-    if ((playerSelection.toLowerCase() === choices[0] && computerDecision === choices[2]) || (playerSelection.toLowerCase() === choices[1] && computerDecision === choices[0]) || (playerSelection.toLowerCase() === choices[2] && computerDecision === choices[1]) ) {
+    if ((playerSelection === choices[0] && computerDecision === choices[2]) || (playerSelection === choices[1] && computerDecision === choices[0]) || (playerSelection === choices[2] && computerDecision === choices[1]) ) {
 
         humanScore++
-        let score = "HUMANIDAD = " + humanScore + " || MAQUINAS = " + computerScore + " || EMPATES = " + tieScore;
-        console.log("TU HAS ELEGIDO " + playerSelection.toUpperCase());
-        console.log("LA COMPUTADORA HA ELEGIDO " + computerDecision.toUpperCase());
-        console.log(ganar);
-        console.log(score);
+        //let score = "HUMANIDAD = " + humanScore + " || MAQUINAS = " + computerScore + " || EMPATES = " + tieScore;
+        // console.log("TU HAS ELEGIDO " + playerSelection.toUpperCase());
+        // console.log("LA COMPUTADORA HA ELEGIDO " + computerDecision.toUpperCase());
+        const p = document.createElement('p');
+        p.innerText = `HAS ELEGIDO ${playerSelection} Y LA COMPUTADORA ${computerDecision}\n` + ganar;
+        outcomeDiv.appendChild(p);
+        //console.log(score);
         
-    } else if (playerSelection.toLowerCase() === computerSelection[computerChoice]) {
+    } else if (playerSelection === computerDecision) {
 
         tieScore++
-        let score = "HUMANIDAD = " + humanScore + " || MAQUINAS = " + computerScore + " || EMPATES = " + tieScore;
-        console.log("TU HAS ELEGIDO " + playerSelection.toUpperCase());
-        console.log("LA COMPUTADORA HA ELEGIDO " + computerDecision.toUpperCase());
-        console.log(empatar);
-        console.log(score);
+        // let score = "HUMANIDAD = " + humanScore + " || MAQUINAS = " + computerScore + " || EMPATES = " + tieScore;
+        // console.log("TU HAS ELEGIDO " + playerSelection.toUpperCase());
+        // console.log("LA COMPUTADORA HA ELEGIDO " + computerDecision.toUpperCase());
+        const p = document.createElement('p')
+        p.innerText = `HAS ELEGIDO ${playerSelection} Y LA COMPUTADORA ${computerDecision}\n` + empatar;
+        outcomeDiv.appendChild(p)
+        //console.log(score);
 
     } else {
 
         computerScore++
-        let score = "HUMANIDAD = " + humanScore + " || MAQUINAS = " + computerScore + " || EMPATES = " + tieScore;
-        console.log("TU HAS ELEGIDO " + playerSelection.toUpperCase());
-        console.log("LA COMPUTADORA HA ELEGIDO " + computerDecision.toUpperCase());
-        console.log(perder);
-        console.log(score);
+        // let score = "HUMANIDAD = " + humanScore + " || MAQUINAS = " + computerScore + " || EMPATES = " + tieScore;
+        // console.log("TU HAS ELEGIDO " + playerSelection.toUpperCase());
+        // console.log("LA COMPUTADORA HA ELEGIDO " + computerDecision.toUpperCase());
+        const p = document.createElement('p')
+        p.innerText = `HAS ELEGIDO ${playerSelection} Y LA COMPUTADORA ${computerDecision}\n` + perder;
+        outcomeDiv.appendChild(p)
+        // console.log(score);
 
     }
 
 };
 
-//GUERRA
-function game() {
-    
- for (let i = 0; i <= 4; i++) {
+const updateScore = (humanScore, computerScore) => {
+    playerScoreSpan.innerText = `Humanidad: ${humanScore}  `;
+    computerScoreSpan.innerText = `|| Maquinas: ${computerScore}`;
+};
 
-    playerSelection = prompt("EL DESTINO DEL UNIVERSO DEPENDE DE TU ELECCION....ELIGE SABIAMENTE O EL MUNDO MORIRA");
-    computerSelection = ["piedra", "papel", "tijera"];
-    computerChoice = Math.floor(Math.random() * computerSelection.length);
-    computerDecision = computerSelection[computerChoice];
-    
-    playRound(playerSelection, computerDecision); 
-          
- };
- 
-    if (humanScore > computerScore){
-        return "HAS GANADO LA GUERRA!!! POR FIN HEMOS DERROTADO A LAS MAQUINAS DEFINITIVAMENTE!!!!!!!";
+const checkWinner = (humanScore, computerScore) => {
+    if (humanScore === 5){
+        const h2 = document.createElement('h2');
+        h2.innerText = `HAS GANADO LA GUERRA ${humanScore} a ${computerScore}!!! POR FIN HEMOS DERROTADO A LAS MAQUINAS DEFINITIVAMENTE!!!!!!!`;;
+        resultDiv.appendChild(h2);
+
     }
-    if (computerScore > humanScore) {
-     return "HEMOS PERDIDO LA GUERRA...YA NADA QUEDA EN PIE";
+    if (computerScore === 5) {
+        const h2 = document.createElement('h2');
+        h2.innerText = `HEMOS PERDIDO LA GUERRA ${computerScore} a ${humanScore}...YA NADA QUEDA EN PIE`;
+        resultDiv.appendChild(h2);
     }
-    else {
-     return "NINGUN BANDO HA PODIDO SACAR VENTAJA. REAGRUPANDO FUERZAS PARA LA PROXIMA BATALLA";
-    }
-}
+};
+
+rockButton.addEventListener('click', () => {
+    const computerDecision = computerPlay();
+    const playerSelection = 'piedra';
+    playRound(playerSelection, computerDecision);
+    updateScore(humanScore, computerScore);
+    checkWinner(humanScore, computerScore);
+
+});
+
+paperButton.addEventListener('click', () => {
+    const computerDecision = computerPlay();
+    const playerSelection = 'papel';
+    playRound(playerSelection, computerDecision);
+    updateScore(humanScore, computerScore);
+    checkWinner(humanScore, computerScore);
+
+});
+
+scissorsButton.addEventListener('click', () => {
+    const computerDecision = computerPlay();
+    const playerSelection = 'tijera';
+    playRound(playerSelection, computerDecision);
+    checkWinner(humanScore, computerScore);
+
+});
